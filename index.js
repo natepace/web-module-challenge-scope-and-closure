d830fe3d53ed1314d83f68009a27a4fb86e1e154
natepace <n8wpace@gmail.com>
2021-01-25 21:09:41 -0700

    did work on problems, answered questions

--- index.js
+++ index.js
@@ -29,11 +29,16 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   
   1. What is the difference between counter1 and counter2?
   
-  2. Which of the two uses a closure? How can you tell?
+    counter1 code has a closure inside. 
   
+  2. Which of the two uses a closure? How can you tell?
+
+    counter 1 has a closure, because counter reaches into countermaker , its parent, to get the variable count.
+
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
-*/
+      closures are useful for object data privacy. 
+     */
 
 // counter1 code
 function counterMaker() {
@@ -62,8 +67,9 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+return Math.floor(Math.random() * 3);
+
 }
 
 
@@ -81,17 +87,32 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(scoreCB, totInnings){
+  return {
+    Home: scoreCB(),
+    Away: scoreCB()
+  }
 }
+
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(inningCB) {
+  const InningScr = [];
+  let homeScore = 0;
+  let awayScore = 0;
+
+  for (let i = 0; i < 10; i++){
+    const currentScores = scoreCB(gamecb);
+    homeScore = homeScore + currentScores.Home
+    awayScore = awayScore + currentScores.Away
+    InningScr.push(``)
+
+  }
+
 }
 
 
